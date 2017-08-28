f230834024d52934618f126ce7d34d91ad70c8a5
ravik9 <31178347+ravik9@users.noreply.github.com>
2017-08-28 09:04:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,10 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
- 
-app.get('/ravi1', function(req, res){
-    res.send('ravi1 will be served here');
-});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
